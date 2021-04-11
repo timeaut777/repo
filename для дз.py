577db197b1ed152ea6bf9d6901d4212f05702147
timeaut777 <makes777@mail.ru>
2021-04-11 14:24:08 +0300

    делам домашку 4.2

--- для дз.py
+++ для дз.py
@@ -1,11 +1,19 @@
 vid = int(input())
-s = 0
+max1 = 0
+max2 = 0
+max3 = 0
 for k in range(vid):
     cv = input()
     kol = int(input())
-    while vid >= 3:
-        if kol > s:
-            s = kol
-print(s)
+    if kol > max1:
+        max2 = max1
+        max1 = kol
+    elif kol > max2:
+        max3 = max2
+        max2 = kol
+    elif kol > max3:
+        max3 = kol
+
+print(max1, max2, max3)
 
 
