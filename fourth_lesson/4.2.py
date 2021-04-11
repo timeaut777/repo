26e7adf5e5b9a8a496bac04cb5d3cec79a853fbc
timeaut777 <makes777@mail.ru>
2021-04-11 14:49:55 +0300

    готовое дз 4.2

--- fourth_lesson/4.2.py
+++ fourth_lesson/4.2.py
@@ -1,23 +1,28 @@
-p = int(input())
-vid = list()
-kol = list()
-maxIndex = 0
-max = 0
-c = 3
+vid = int(input())
+max1 = 0
+max2 = 0
+max3 = 0
+max1cv = str
+max2cv = str
+max3cv = str
 
-for k in range(p):
-    a = input("Введите вид цветов: ")
-    b = input("Введите количество цветов данного вида: ")
-    vid.append(a)
-    kol.append(int(b))
+for k in range(vid):
+    cv = input()
+    kol = int(input())
+    if kol > max1:
+        max3 = max2
+        max3cv = max2cv
+        max2 = max1
+        max2cv = max1cv
+        max1 = kol
+        max1cv = cv
+    elif kol > max2:
+        max3 = max2
+        max3cv = max2cv
+        max2 = kol
+        max2cv = cv
+    elif kol > max3:
+        max3 = kol
+        max3cv = cv
 
-while c != 0:
-    for i in range(len(kol)):
-        if kol[i] > max:
-           max = kol[i]
-           maxIndex = i
-    print(vid[maxIndex])
-    del kol[maxIndex]
-    del vid[maxIndex]
-    max = 0
-    c -= 1
+print(max1cv, max2cv, max3cv, sep='\n')
