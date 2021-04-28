f8357fb898925d811615d45481b526161c80d352
timeaut777 <makes777@mail.ru>
2021-04-28 14:17:27 +0300

    всё ещё в процессе 5.1

--- fifth_lesson/5.1.py
+++ fifth_lesson/5.1.py
@@ -2,13 +2,9 @@ k = int(input())
 
 for i in range(k):
     f = [int(i) for i in input().split()]
-
-a = []
-for j in f:
-    if j == j+1:
-        a.append(j)
-    else:
-        a.append(j)
-        a.append(j+1)
+    for j in f:
+        a = []
+        if j not in a:
+            a.append(j)
 
 print(a)
