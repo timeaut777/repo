98a324fd0cb1185bce26e74b0c8e5bcd6b739064
timeaut777 <makes777@mail.ru>
2021-05-02 15:06:16 +0300

    готово дз 4.3

---
+++ fifth_lesson/5.5.py
@@ -0,0 +1,38 @@
+command = 0
+a = []
+b = []
+while command != 'Stop':
+    command = input()
+
+    if command == 'Добавить новую фамилию':
+        f = input()
+        if f in a:
+            print('ggg')
+        else:
+            a.append(f)
+            b.append(1)
+    print(a, b)
+
+    if command == 'Удалить по индексу':
+        index = int(input())
+        if index < len(b):
+            b[index] = 0
+        else:
+            print('В списке нет такого индекса')
+    print(a, b)
+
+    if command == 'Удалить по фаммилии':
+        f = input()
+        if f in a:
+            for i in range(len(a)-1):
+                if f == a[i]:
+                    b[i] = 0
+        else:
+            print('В списке нет такой фамилии')
+    print(a, b)
+
+
+
+
+
+
