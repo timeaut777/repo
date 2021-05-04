e5a51f78db5432260825136fe3e70deda168d74f
timeaut777 <makes777@mail.ru>
2021-05-04 21:04:02 +0300

    5.5 вариант Макса

---
+++ fifth_lesson/5.5(вариант Макса).py
@@ -0,0 +1,33 @@
+n = 0
+f = []
+d = []
+
+while n != 'Stop':
+    n = input()
+
+    if 'Добавить новую фамилию' in n:
+        n = n.replace('Добавить новую фамилию ', '')
+        if n in f:
+            print('Фамилия уже есть в списке')
+        else:
+            f.append(n)
+            d.append(1)
+        print(f, d)
+
+    if 'Удалить по фамилии' in n:
+        n = n.replace('Удалить по фамилии ', '')
+        if n in f:
+            index = f.index(n)
+            d[index] = 0
+            print(f, d)
+        else:
+            print('В списке нет такой фамилии')
+
+    if 'Удалить по индексу' in n:
+        n = n.replace('Удалить по индексу ', '')
+        n = int(n)
+        if n > len(f)-1:
+            print('В списке нет такого индекса')
+        else:
+            d[n] = 0
+            print(f, d)
