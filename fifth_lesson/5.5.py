b927b7add770719263f27140192a871966bb4eb2
timeaut777 <makes777@mail.ru>
2021-05-11 08:45:43 +0300

    готово дз 5.5

--- fifth_lesson/5.5.py
+++ fifth_lesson/5.5.py
@@ -1,36 +1,43 @@
 command = 0
+index = int
 a = []
 b = []
+c = []
 while command != 'Stop':
     command = input()
 
-    if command == 'Добавить новую фамилию':
-        f = input()
-        if f in a:
-            pass
-        else:
-            a.append(f)
+    if 'Добавить новую фамилию' in command:
+        command = command.split()
+        command = command[len(command)-1]
+        if command not in a:
+            a.append(command)
             b.append(1)
-    print(a, b)
 
-    if command == 'Удалить по индексу':
-        index = int(input())
+    if 'Удалить по индексу' in command:
+        command = command.split()
+        command = command[len(command) - 1]
+        index = int(command)
         if index < len(b):
             b[index] = 0
         else:
             print('В списке нет такого индекса')
-    print(a, b)
 
-    if command == 'Удалить по фамилии':
-        f = input()
-        if f in a:
-            for i in range(len(a)-1):
-                if f == a[i]:
+    if 'Удалить по фамилии' in command:
+        command = command.split()
+        command = command[len(command) - 1]
+        if command in a:
+            for i in range(len(a)):
+                if command == a[i]:
                     b[i] = 0
-
         else:
             print('В списке нет такой фамилии')
-    print(a, b)
+
+print(a, b, sep='\n')
+
+for j in range(len(b)):
+    if b[j] == 1:
+       c.append(a[j])
+print(c)
 
 
 
