2db957f1d59a0163d3e573eb612a88b27c893d35
timeaut777 <makes777@mail.ru>
2021-05-12 22:50:42 +0300

    готово дз 6.3

--- sixth_lesson/6.3.py
+++ sixth_lesson/6.3.py
@@ -11,26 +11,39 @@ for i in a:
         print(k, end=' ')
     print()
 
-if a[0][0] and a[0][1] and a[0][2] == 'X':
+if a[0][0] == 'X' and a[0][1] == 'X' and a[0][2] == 'X':
+    print('Крестики')
+elif a[1][0] == 'X' and a[1][1] == 'X' and a[1][2] == 'X':
+    print('Крестики')
+elif a[2][0] == 'X' and a[2][1] == 'X' and a[2][2] == 'X':
+    print('Крестики')
+elif a[0][0] == 'X' and a[1][0] == 'X' and a[2][0] == 'X':
+    print('Крестики')
+elif a[0][1] == 'X' and a[1][1] == 'X' and a[2][1] == 'X':
+    print('Крестики')
+elif a[0][2] == 'X' and a[1][2] == 'X' and a[2][2] == 'X':
+    print('Крестики')
+elif a[0][0] == 'X' and a[1][1] == 'X' and a[2][2] == 'X':
+    print('Крестики')
+elif a[0][2] == 'X' and a[1][1] == 'X' and a[2][0] == 'X':
     print('Крестики')
 
-if a[1][0] and a[1][1] and a[1][2] == 'X':
-    print('Крестики')
+elif a[0][0] == '0' and a[0][1] == '0' and a[0][2] == '0':
+    print('Нолики')
+elif a[1][0] == '0' and a[1][1] == '0' and a[1][2] == '0':
+    print('Нолики')
+elif a[2][0] == '0' and a[2][1] == '0' and a[2][2] == '0':
+    print('Нолики')
+elif a[0][0] == '0' and a[1][0] == '0' and a[2][0] == '0':
+    print('Нолики')
+elif a[0][1] == '0' and a[1][1] == '0' and a[2][1] == '0':
+    print('Нолики')
+elif a[0][2] == '0' and a[1][2] == '0' and a[2][2] == '0':
+    print('Нолики')
+elif a[0][0] == '0' and a[1][1] == '0' and a[2][2] == '0':
+    print('Нолики')
+elif a[0][2] == '0' and a[1][1] == '0' and a[2][0] == '0':
+    print('Нолики')
 
-if a[2][0] and a[2][1] and a[2][2] == 'X':
-    print('Крестики')
-
-if a[0][0] and a[1][0] and a[2][0] == 'X':
-    print('Крестики')
-
-if a[0][1] and a[1][1] and a[2][1] == 'X':
-    print('Крестики')
-
-if a[0][2] and a[1][2] and a[2][2] == 'X':
-    print('Крестики')
-
-if a[0][0] and a[1][1] and a[2][2] == 'X':
-    print('Крестики')
-
-if a[0][2] and a[1][1] and a[2][0] == 'X':
-    print('Крестики')
+else:
+    print('Никто не победил')
