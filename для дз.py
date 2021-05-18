82c3219fde1d4a36443c4396ba1d884b656ec167
timeaut777 <makes777@mail.ru>
2021-05-18 10:54:23 +0300

    типа 7.1

--- для дз.py
+++ для дз.py
@@ -1,26 +1,6 @@
-dlin = int(input())
-a = 0
-j = 0
-s = 0
-k = 0
-max = 0
+# 7.1
 
-for i in range(dlin//100):
-    v = int(input())
-    while v > a:
-        a = v
-        j += 1
-    if v <= a:
-        a = v
-        s = j
-
-    if s > j:
-        max = s
-    else:
-        max = j
-
-    j = 0
-
-print(max)
-
-
+a = input()
+a = a.split()
+b = ' '.join(a)
+print(b)
